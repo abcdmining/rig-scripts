eeeb6b114b7b4a823e2d2eb9a2e3b243e0d6b3c8
Ben <info@abcdmining.com>
2018-02-12 21:51:05 -0500

    update

--- 13-1070Ti-settings.sh
+++ 13-1070Ti-settings.sh
@@ -3,10 +3,7 @@
 #!/bin/bash
 
 clockOffset="235"
-memoryOffset="200"
-
-nvidia-xconfig --enable-all-gpus
-nvidia-xconfig --cool-bits=12
+memoryOffset="300"
 
 nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=85
 nvidia-settings -a [gpu:1]/GPUFanControlState=1 -a [fan:1]/GPUTargetFanSpeed=85
@@ -31,11 +28,11 @@ nvidia-smi -i 4 -pl 120
 nvidia-smi -i 5 -pl 120
 nvidia-smi -i 6 -pl 120
 nvidia-smi -i 7 -pl 120
-nvidia-smi -i 8 -pl 170
-nvidia-smi -i 9 -pl 170
-nvidia-smi -i 10 -pl 170
-nvidia-smi -i 11 -pl 170
-nvidia-smi -i 12 -pl 170
+nvidia-smi -i 8 -pl 180
+nvidia-smi -i 9 -pl 180
+nvidia-smi -i 10 -pl 180
+nvidia-smi -i 11 -pl 180
+nvidia-smi -i 12 -pl 180
 
 nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=$memoryOffset
