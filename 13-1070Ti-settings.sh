7a035860963c1b65981aaafcc685c6eae79d5c15
Ben <info@abcdmining.com>
2018-02-13 17:52:45 -0500

    update

--- 13-1070Ti-settings.sh
+++ 13-1070Ti-settings.sh
@@ -35,38 +35,50 @@ nvidia-smi -i 9 -pl $highWatt
 nvidia-smi -i 10 -pl $highWatt
 nvidia-smi -i 11 -pl $highWatt
 
+nvidia-settings -a [gpu:0]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:1]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:1]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:2]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:2]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:2]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:3]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:3]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:3]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:4]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:4]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:4]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:5]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:5]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:5]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:6]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:6]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:6]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:7]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:7]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:7]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:8]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:8]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:8]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:9]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:9]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:9]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:10]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:10]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:10]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
+nvidia-settings -a [gpu:11]/GpuPowerMizerMode=2
 nvidia-settings -a [gpu:11]/GPUGraphicsClockOffset[3]=$clockOffset
 nvidia-settings -a [gpu:11]/GPUMemoryTransferRateOffset[3]=$memoryOffset
