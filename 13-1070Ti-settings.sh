c0b00ca6d7903e4910bc510af6c93da376fac122
Ben <info@abcdmining.com>
2018-02-13 04:18:07 -0500

    update

--- 13-1070Ti-settings.sh
+++ 13-1070Ti-settings.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 clockOffset="200"
-memoryOffset="100"
+#memoryOffset="100"
 lowWatt="120"
 highWatt="180"
 
@@ -36,37 +36,37 @@ nvidia-smi -i 10 -pl $highWatt
 nvidia-smi -i 11 -pl $highWatt
 
 nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:1]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:2]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:2]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:2]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:3]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:3]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:3]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:4]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:4]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:4]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:5]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:5]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:5]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:6]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:6]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:6]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:7]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:7]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:7]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:8]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:8]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:8]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:9]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:9]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:9]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:10]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:10]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:10]/GPUMemoryTransferRateOffset[3]=$memoryOffset
 
 nvidia-settings -a [gpu:11]/GPUGraphicsClockOffset[3]=$clockOffset
-nvidia-settings -a [gpu:11]/GPUMemoryTransferRateOffset[3]=$memoryOffset
+#nvidia-settings -a [gpu:11]/GPUMemoryTransferRateOffset[3]=$memoryOffset
